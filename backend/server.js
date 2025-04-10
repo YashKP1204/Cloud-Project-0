71e4c63e35da18fa2ef0320dbca9da0722a0ae2c
YashKP1204 <yashprajapati121204@gmail.com>
2025-04-10 17:03:54 +0530

    docker-setup -V0 : setting up configuration for containerization , deploy-phase-1 - ec2-instance

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose")
 const app = express();
+const authRoutes = require("./src/routes/authRoutes"); 
+const userRoutes = require("./src/routes/userRoutes"); 
 
-const authRoutes = require("./routes/authRoutes"); 
-const userRoutes = require("./routes/userRoutes"); 
-const productRoutes = require("./routes/productRoutes"); 
+
 
 // middleware
 app.use(cors());
@@ -20,7 +21,17 @@ app.get("/",(req,res)=>{
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api/products", productRoutes);
+// app.use("/api/products", productRoutes);
 const PORT = process.env.PORT || 5000;
+
+const mongoConnect = async()=>{
+    mongoose.connect(process.env.MONGO_URI);
+}
+
+mongoConnect().then(()=>{
+    console.log("Successfully connected to the database");
+}).catch((err)=>{
+    console.log("Error detected to connect db: ", err);    
+})
 
 app.listen(PORT,()=>console.log(`server running on port ${PORT}`));
--- backend/src/controllers/profileController.js
+++ backend/src/controllers/profileController.js
@@ -1,4 +1,4 @@
-const User = require("../models/userModel");
+const User = require("../models/User");
 
 // @desc    Fetch user profile
 // @route   GET /api/user/profile
